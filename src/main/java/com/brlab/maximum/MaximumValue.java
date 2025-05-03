d24430af86ef88402ff9ddef1fbcce7dc3410e3e
pavan0628 <pavan.cse.rymec@gmail.com>
2025-05-03 15:04:10 +0530

    [PAVAN]UPDATED:Added new method to find maximum of three float values

--- src/main/java/com/brlab/maximum/MaximumValue.java
+++ src/main/java/com/brlab/maximum/MaximumValue.java
@@ -13,4 +13,15 @@ public class MaximumValue {
         }
          return max;
     }
+    static Float FindMaxFloat(Float a, Float b,Float c){
+        Float max=a;
+
+        if(b.compareTo(max)>0){
+            max=b;
+        }
+        if(c.compareTo(max)>0){
+            max=c;
+        }
+        return max;
+    }
 }
