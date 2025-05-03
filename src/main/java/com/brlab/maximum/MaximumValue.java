7113723e00a627a7989baae76183d4dedda09540
pavan0628 <pavan.cse.rymec@gmail.com>
2025-05-03 16:19:06 +0530

    [PAVAN]REFACTORED:refactored the find maximum method by making the method genric type

--- src/main/java/com/brlab/maximum/MaximumValue.java
+++ src/main/java/com/brlab/maximum/MaximumValue.java
@@ -2,8 +2,8 @@ package com.brlab.maximum;
 
 public class MaximumValue {
 
-    static Integer findMaxInt(Integer a, Integer b,Integer c){
-        Integer max=a;
+    static <E extends Comparable>Comparable findMaximum(E a, E b, E c){
+        E max=a;
 
         if(b.compareTo(max)>0){
             max=b;
@@ -13,26 +13,5 @@ public class MaximumValue {
         }
          return max;
     }
-    static Float findMaxFloat(Float a, Float b,Float c){
-        Float max=a;
 
-        if(b.compareTo(max)>0){
-            max=b;
-        }
-        if(c.compareTo(max)>0){
-            max=c;
-        }
-        return max;
-    }
-    static String findMaxString(String a, String b,String c){
-        String max=a;
-
-        if(b.compareTo(max)>0){
-            max=b;
-        }
-        if(c.compareTo(max)>0){
-            max=c;
-        }
-        return max;
-    }
 }
