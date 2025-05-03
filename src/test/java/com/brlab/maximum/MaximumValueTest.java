15ae209085680ea9ee821d83cfd39bd5e1650053
pavan0628 <pavan.cse.rymec@gmail.com>
2025-05-03 14:54:40 +0530

    [PAVAN]UPDATED:Updated by adding test case for maximum at second  position

--- src/test/java/com/brlab/maximum/MaximumValueTest.java
+++ src/test/java/com/brlab/maximum/MaximumValueTest.java
@@ -20,5 +20,12 @@ class MaximumValueTest {
 
     }
 
+    @Test
+    void findMaxTestSecondPosition(){
+
+        Assertions.assertEquals(90,MaximumValue.FindMaxInt(10,90,20),"Test case failed because the excpected output is 90");
+
+    }
+
   
 }
